4d0f82bcf49052feeca73fe85fff294e85ac1448
swiecone <alejandro.swiec@gmail.com>
2016-02-08 00:36:47 +0100

    Followed blog getting started and added links and edit views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,9 +15,22 @@ class ArticlesController < ApplicationController
 	def create
 		@article = Article.new(article_params)
 
-		@article.save
-		redirect_to @article
+		if @article.save
+			redirect_to @article
+		else 
+			render 'new'
+		end
 	end 
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end 
+	end
 
 	private
 		def article_params
